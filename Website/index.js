41fc86eb1d2f6cd0428ac5133f220ab8d12898e0
Cosmo R <der.computerfuchs@gmail.com>
2021-10-23 22:10:13 +0200

    sexy

--- Website/index.js
+++ Website/index.js
@@ -1,16 +1,20 @@
 const socket = io("127.0.0.1:8080");
 
+// !127.0.0.1 == localhost
+
 const wordConfirm = document.getElementById("wordConfirm");
+const wordConfirmDisabled = document.getElementById("wordConfirmDisabled");
 const wordInput = document.getElementById("wordInput");
 const currentSentence = document.getElementById("currentSentence");
 
 wordConfirm.addEventListener("click", handleWordConfirm);
 
-let test = "hello";
-
-test.toLowerCase();
+socket.on("test", (msg) => {
+  console.log(msg);
+});
 
 function handleWordConfirm() {
+  enableButton();
   if (wordInput.value) {
     if (currentSentence.innerText.length === 0) {
       currentSentence.innerText = `${currentSentence.innerText} ${wordInput.value}`;
@@ -20,7 +24,18 @@ function handleWordConfirm() {
       } ${wordInput.value.toLowerCase()}`;
     }
     wordInput.value = "";
+    // disableButton();
   } else {
     alert("Please Input something bruh");
   }
+}
+
+function disableButton() {
+  wordConfirm.style.display = "none";
+  wordConfirmDisabled.style.display = "block";
+}
+
+function enableButton() {
+  wordConfirm.style.display = "block";
+  wordConfirmDisabled.style.display = "none";
 }
